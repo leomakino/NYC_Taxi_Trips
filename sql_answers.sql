3861c602f05fbce0870b379cdfb327dd4f2b43d0
Leonardo <leomakino@yahoo.com.br>
2021-10-21 20:59:30 -0300

    Atualiza sql_answers

--- sql_answers.sql
+++ sql_answers.sql
@@ -1,5 +1,5 @@
 --Question 1---
-SELECT AVG(all_trips.trip_distance) AS avg_trip_distance
+SELECT ROUND(AVG(all_trips.trip_distance),3) AS avg_trip_distance
 FROM (
     SELECT 2009_trips_table.trip_distance
     FROM 2009_trips_table
@@ -19,7 +19,7 @@ FROM (
 ) AS all_trips;
 
 --Question 2--
-SELECT SUM(all_trips.total_amount) AS total_amount, name
+SELECT name, SUM(all_trips.total_amount)
 FROM (
     SELECT 2009_trips_table.total_amount, 2009_trips_table.vendor_id
     FROM 2009_trips_table
@@ -36,11 +36,11 @@ FROM (
 INNER JOIN vendor_lookup
 ON all_trips.vendor_id = vendor_lookup.vendor_id
 GROUP BY name
-ORDER BY total_amount DESC
+ORDER BY all_trips.total_amount DESC
 LIMIT 3;
 
 --Question 3
-SELECT pickup_datetime, COUNT(pl.payment_lookup)
+SELECT TO_TIMESTAMP(pickup_datetime, 'YYYY-MM-DDTHH24:MI:SS.US+Z') AS pickup_datetime_converted, COUNT(pl.payment_lookup)
 FROM (
     SELECT 2009_trips_table.pickup_datetime, 2009_trips_table.payment_type
     FROM 2009_trips_table
@@ -56,4 +56,6 @@ FROM (
 ) AS all_trips
 INNER JOIN payment_lookup-csv AS pl
 ON all_trips.payment_type = pl.payment_type
-GROUP BY pickup_datetime;
+WHERE payment_lookup = 'Cash'
+GROUP BY pickup_datetime_converted;
+                                                                                  "Cash"
